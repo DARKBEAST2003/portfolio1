c0f79245aba7f97b842c91bb320b5382a8fe6f67
DARKBEAST2003 <ruzni2003@gmail.com>
2025-07-05 14:37:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+};
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+            });
+        };
+    });
+
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    menuIcon.classList.remove("fa-xmark");
+    navbar.classList.remove("active");    
+};
+
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 1500,
+    delay: 150
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: "top" });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: "bottom" });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: "left" });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: "right" });
+
+const typed = new Typed(".multiple-text", {
+    strings: ["Fullstack Developer", "Software Developer", "Designer", "Software Engineer", "Cyber Protecter"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function toggleText() {
+  const moreText = document.getElementById("moreText");
+  const btn = document.getElementById("toggleBtn");
+
+  if (moreText.style.display === "none" || moreText.style.display === "") {
+    moreText.style.display = "inline";
+    btn.innerText = "Read Less";
+  } else {
+    moreText.style.display = "none";
+    btn.innerText = "Read More";
+  }
+}
---
+++ script2.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 1500,
+    delay: 150
+});
+
+ScrollReveal().reveal('.top', { origin: "top" });
+ScrollReveal().reveal('.bot', { origin: "bottom" });
+ScrollReveal().reveal('.left', { origin: "left" });
+ScrollReveal().reveal('.right', { origin: "right" });
