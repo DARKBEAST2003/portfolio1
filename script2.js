6dbf575f89b59f4490517bf06b01c10808ec9d19
DARKBEAST2003 <ruzni2003@gmail.com>
2025-07-20 22:40:06 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -9,3 +9,15 @@ ScrollReveal().reveal('.top', { origin: "top" });
 ScrollReveal().reveal('.bot', { origin: "bottom" });
 ScrollReveal().reveal('.left', { origin: "left" });
 ScrollReveal().reveal('.right', { origin: "right" });
+
+const themeSwitch = document.getElementById("themeSwitch");
+const toggle = document.getElementById('themeSwitch');
+const label = document.getElementById('modeLabel');
+
+themeSwitch.addEventListener("change", () => {
+    document.body.classList.toggle("light", !themeSwitch.checked);
+});
+
+toggle.addEventListener('change', () => {
+    label.innerText = !toggle.checked ? 'LIGHT MODE' : 'DARK MODE';
+});
